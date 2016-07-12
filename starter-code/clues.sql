8d3786e4f0c4febfa4a7beec4a604dccb57c95c2
StacyZol <stacyzolnikov@Stacy-Zolnikovs-MacBook-Pro.local>
2016-07-12 08:30:20 -0400

    Finished lab

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,42 +2,67 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+select code character, name, population from country where region like 'Southern Europe' order by population desc;
+Answer: ITA|Italy, population: 57680000
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+select language, isofficial from countrylanguage where countrycode like 'ITA';
+Answer: Italian
+Italian|t
+Sardinian|f
+Friuli|f
+French|f
+German|f
+Albaniana|f
+Slovene|f
+Romani|f
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+select * from countrylanguage where language like 'Italian' and isofficial like 't';
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
+select * from countrylanguage where language = 'Italian' and percentage = 100;
+Answer: SMR|Italian|t|100.0
 
+select * from country where code like 'SMR';
+SMR|San Marino|Europe|Southern Europe|61.0|885|27000|81.099998|510|\N|San Marino|Republic|\N|3171|SM
 
+select name from city where countrycode = 'SMR';
+Serravalle
+San Marino
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+select * from city where name like 'Serra%';
+Answer = 265|Serra|BRA|Esp�rito Santo|302666
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+select * from country where code like 'BRA';
+BRA|Brazil|South America|South America|8547403.0|1822|170115000|62.900002|776739|804108|Brasil|Federal Republic|Fernando Henrique Cardoso|211|BR
+select capital from country where code like 'BRA';
+211
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
+  select name from city where id = 211;
+Bras�lia
+sqlite> 
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
@@ -48,10 +73,11 @@
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
-
+select name from city where population = 91084;
+Santa Monica
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
--- She's in ____________________________!
+She's in Santa Monica!
